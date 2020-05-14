032e71fac404f75845aea257398d7f2a0ed4111a
Admin <777programmer777@gmil.com>
2020-05-14 12:49:20 +0300

    Добавьте файлы проекта.

---
+++ Banker's Algorithm/Banker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banker_s_Algorithm
+{
+    class Banker
+    {
+
+    }
+}
---
+++ Banker's Algorithm/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Banker_s_Algorithm
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
