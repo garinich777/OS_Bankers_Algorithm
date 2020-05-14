3d9c89275dbec41aeaf0b5b4beb50c88658160ea
Admin <777programmer777@gmil.com>
2020-05-14 13:52:20 +0300

    Реализован пользовательский ввод

--- Banker's Algorithm/Program.cs
+++ Banker's Algorithm/Program.cs
@@ -23,32 +23,18 @@ namespace Banker_s_Algorithm
                 resourse[i] = IntputData($"ресурс {i + 1}:");
 
             int process_count = IntputData("Введите кол-во процессов:");
-            int[,] process = new int[process_count, resource_count * 2];
-            for (int i = 0; i < process_count; i++)
-            {
-                Console.WriteLine($"{Environment.NewLine}Предоставлено ресурсов:");
-                for (int j = 0; j < resource_count; j++)
-                    process[i, j] = IntputData($"процесс {i + 1}, ресурс {j + 1}:");
-                Console.WriteLine($"{Environment.NewLine}Максимальная потребность:");
-                for (int j = resource_count; j < resource_count * 2; j++)
-                    process[i, j] = IntputData($"процесс {i + 1}, ресурс {j - resource_count + 1}:");
-            }
-
             int[,] allocated_resors_process = new int[resource_count, process_count];
             int[,] max_resors_process = new int[resource_count, process_count];
 
             Console.WriteLine($"{Environment.NewLine}Предоставлено ресурсов:");
-            for (int j = 0; j < resource_count; j++)
-                for (int i = 0; i < process_count; i++)
-                    process[i, j] = IntputData($"процесс {i + 1}, ресурс {j + 1}:");
+            for (int i = 0; i < process_count; i++)
+                for (int j = 0; j < resource_count; j++)                
+                    allocated_resors_process[j, i] = IntputData($"-процесс A{i + 1}, ресурс R{j + 1}:");
 
-                
-
-
-
-            Console.Write("Введите кол-во ресурсов:");
-
-            Console.WriteLine("Введите потоки");
+            Console.WriteLine($"{Environment.NewLine}Максимальная потребность:");
+            for (int i = 0; i < process_count; i++)
+                for (int j = 0; j < resource_count; j++)                
+                    max_resors_process[j, i] = IntputData($"-процесс A{i + 1}, ресурс R{j + 1}:");
         }
     }
 }
